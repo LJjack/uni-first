a1fab0f1ed9e0bf8527ca1b2fd2ac29d1e97c1b6
刘俊杰 <945980335@qq.com>
2022-07-22 15:33:40 +0800

    结算

--- components/my-settle/my-settle.vue
+++ components/my-settle/my-settle.vue
@@ -2,7 +2,7 @@
 	<!-- 最外层的容器 -->
 	<view class="my-settle-container">
 		<!-- 全选区域 -->
-		<label class="radio"  @click="changeAllState">
+		<label class="radio" @click="changeAllState">
 			<radio color="#C00000" :checked="isFullCheck" /><text>全选</text>
 		</label>
 
@@ -12,19 +12,24 @@
 		</view>
 
 		<!-- 结算按钮 -->
-		<view class="btn-settle">结算({{checkedCount}})</view>
+		<view class="btn-settle" @click="settlement">结算({{checkedCount}})</view>
 	</view>
 </template>
 
 <script>
 	import {
 		mapGetters,
-		mapMutations
+		mapMutations,
+		mapState,
 	} from 'vuex'
 	export default {
 		name: "my-settle",
 		computed: {
-			...mapGetters('m_cart', ['checkedCount', 'total','checkedGoodsAmount']),
+			...mapGetters('m_cart', ['checkedCount', 'total', 'checkedGoodsAmount']),
+			// addstr 是详细的收货地址
+			...mapGetters('m_user', ['addstr']),
+			// token 是用户登录成功之后的 token 字符串
+			...mapState('m_user', ['token']),
 			isFullCheck() {
 				return this.total === this.checkedCount
 			},
@@ -38,11 +43,22 @@
 			// 使用 mapMutations 辅助函数，把 m_cart 模块提供的 updateAllGoodsState 方法映射到当前组件中使用
 			...mapMutations('m_cart', ['updateAllGoodsState']),
 			// label 的点击事件处理函数
-			  changeAllState() {
-			    // 修改购物车中所有商品的选中状态
-			    // !this.isFullCheck 表示：当前全选按钮的状态取反之后，就是最新的勾选状态
-			    this.updateAllGoodsState(!this.isFullCheck)
-			  }
+			changeAllState() {
+				// 修改购物车中所有商品的选中状态
+				// !this.isFullCheck 表示：当前全选按钮的状态取反之后，就是最新的勾选状态
+				this.updateAllGoodsState(!this.isFullCheck)
+			},
+			// 点击了结算按钮
+			settlement() {
+				// 1. 先判断是否勾选了要结算的商品
+				if (!this.checkedCount) return uni.$showMsg('请选择要结算的商品！')
+
+				// 2. 再判断用户是否选择了收货地址
+				if (!this.addstr) return uni.$showMsg('请选择收货地址！')
+
+				// 3. 最后判断用户是否登录了
+				if (!this.token) return uni.$showMsg('请先登录！')
+			}
 		}
 	}
 </script>
--- mixins/tabbar-badge.js
+++ mixins/tabbar-badge.js
@@ -20,11 +20,18 @@ export default {
 	
 	methods: {
 	   setBadge() {
-	      // 调用 uni.setTabBarBadge() 方法，为购物车设置右上角的徽标
-	      uni.setTabBarBadge({
-	         index: 2, // 索引
-	         text: this.total == 0 ? null : this.total + '' // 注意：text 的值必须是字符串，不能是数字
-	      })
+		   if (this.total > 0) {
+			   // 调用 uni.setTabBarBadge() 方法，为购物车设置右上角的徽标
+			   uni.setTabBarBadge({
+			      index: 2, // 索引
+			      text: this.total + '' // 注意：text 的值必须是字符串，不能是数字
+			   })
+		   }else {
+			   uni.removeTabBarBadge({
+			   	index:2
+			   })
+		   }
+	      
 	   }
 	}
 	
