a1af2bbb2a187159591683259142549a1dfd2caf
刘俊杰 <945980335@qq.com>
2022-07-22 13:53:01 +0800

    解决购物车角标

--- mixins/tabbar-badge.js
+++ mixins/tabbar-badge.js
@@ -23,7 +23,7 @@ export default {
 	      // 调用 uni.setTabBarBadge() 方法，为购物车设置右上角的徽标
 	      uni.setTabBarBadge({
 	         index: 2, // 索引
-	         text: this.total + '' // 注意：text 的值必须是字符串，不能是数字
+	         text: this.total == 0 ? null : this.total + '' // 注意：text 的值必须是字符串，不能是数字
 	      })
 	   }
 	}
